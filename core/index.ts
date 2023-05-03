eb74eb081d7ac26a612c01c46d89d4fe64e3f020
axm <56112475+ax-sh@users.noreply.github.com>
2023-05-03 21:00:14 +0545

    feat: add api to get google sheet doc

--- core/index.ts
+++ core/index.ts
@@ -1,1 +1,12 @@
 import { GoogleSpreadsheet } from "google-spreadsheet";
+
+export async function fetchGoogleSheetDocWithAccessToken(
+  sheetID: string,
+  accessToken: string
+) {
+  const doc = new GoogleSpreadsheet(sheetID);
+  doc.useRawAccessToken(accessToken);
+  await doc.loadInfo(); // loads document properties and worksheets
+
+  return doc;
+}
