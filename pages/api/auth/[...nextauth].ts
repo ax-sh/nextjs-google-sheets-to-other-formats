8aaac989b39616f0acca69480f3861be2d4d2a01
axm <56112475+ax-sh@users.noreply.github.com>
2023-05-03 20:40:18 +0545

    feat: add next-auth type

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -3,8 +3,8 @@ import GoogleProvider from "next-auth/providers/google";
 import { AuthOptions } from "next-auth/src";
 
 const googleConfig = GoogleProvider({
-  clientId: process.env.GOOGLE_CLIENT_ID as string,
-  clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+  clientId: process.env.GOOGLE_OAUTH_CLIENT_ID as string,
+  clientSecret: process.env.GOOGLE_OAUTH_CLIENT_SECRET as string,
 });
 export const authOptions: AuthOptions = {
   providers: [googleConfig],
