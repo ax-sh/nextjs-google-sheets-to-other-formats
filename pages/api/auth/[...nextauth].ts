dedd05511a9e4b6b0c40802cde47c051b8bb2712
axm <56112475+ax-sh@users.noreply.github.com>
2023-05-03 20:53:39 +0545

    feat: add google oauth scopes

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,12 +2,27 @@ import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { AuthOptions } from "next-auth/src";
 
+export const googleSheetsScopes = [
+  "https://www.googleapis.com/auth/userinfo.email",
+  "https://www.googleapis.com/auth/spreadsheets",
+  "https://www.googleapis.com/auth/drive.readonly",
+];
+
 const googleConfig = GoogleProvider({
   clientId: process.env.GOOGLE_OAUTH_CLIENT_ID as string,
   clientSecret: process.env.GOOGLE_OAUTH_CLIENT_SECRET as string,
+  authorization: {
+    params: {
+      prompt: "consent",
+      access_type: "offline",
+      response_type: "code",
+      scopes: googleSheetsScopes,
+    },
+  },
 });
 export const authOptions: AuthOptions = {
   providers: [googleConfig],
+  secret: "123",
   callbacks: {
     async jwt({ token, account }) {
       // Persist the OAuth access_token to the token right after signin
--- pages/api/sheet/index.ts
+++ pages/api/sheet/index.ts
@@ -3,7 +3,10 @@ import { authOptions } from "../auth/[...nextauth]";
 import { NextApiRequest, NextApiResponse } from "next";
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const session = await getServerSession(req, res, authOptions);
+
   if (session) {
+    const { accessToken } = session;
+    console.log(accessToken, 7788);
     res.send({
       content:
         "This is protected content. You can access this content because you are signed in.",
