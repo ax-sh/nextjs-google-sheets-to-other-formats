f92308ffa4ebf36de3b8acf65ee35a871896f54b
axm <56112475+ax-sh@users.noreply.github.com>
2023-05-03 21:05:19 +0545

    feat: use dynamic route

--- pages/api/sheet/index.ts
+++ pages/api/sheet/index.ts
@@ -1,12 +1,15 @@
 import { getServerSession } from "next-auth/next";
 import { authOptions } from "../auth/[...nextauth]";
 import { NextApiRequest, NextApiResponse } from "next";
-export default async (req: NextApiRequest, res: NextApiResponse) => {
+import { fetchGoogleSheetDocWithAccessToken } from "@/core";
+
+async function handler(req: NextApiRequest, res: NextApiResponse) {
   const session = await getServerSession(req, res, authOptions);
 
   if (session) {
     const { accessToken } = session;
-    console.log(accessToken, 7788);
+    console.log(accessToken, req, 7788);
+    // await fetchGoogleSheetDocWithAccessToken();
     res.send({
       content:
         "This is protected content. You can access this content because you are signed in.",
@@ -17,4 +20,5 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
         "You must be signed in to view the protected content on this page.",
     });
   }
-};
+}
+export default handler;
